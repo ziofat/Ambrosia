72e57b77370f8644780f353d2be22a15f44c8354
ziofat <ziofat@users.noreply.github.com>
2022-04-04 19:14:17 +0000

    deploy: ebb55c6fc394c624a60b8f11f86e98ba3e75d397

---
+++ assets/index.html.02ff755f.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"",lang:"zh-CN",frontmatter:{home:!0,link:"/recipes/"},excerpt:"",headers:[]};export{e as data};
---
+++ assets/recipes.html.123bede0.js
@@ -0,0 +1,1 @@
+const e={key:"v-a0416e66",path:"/recipes.html",title:"",lang:"zh-CN",frontmatter:{finder:"all"},excerpt:"",headers:[]};export{e as data};
