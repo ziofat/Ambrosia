840cfa9c607bc3a6f035dc772fed4a650bd4b321
ziofat <ziofat@users.noreply.github.com>
2022-04-06 14:46:44 +0000

    deploy: 940a58c6e929b2a480859790194d0801b196d664

--- assets/index.html.31e1315c.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-4e5784d5",path:"/recipes/soup/",title:"\u6C64\u54C1",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[{level:2,title:"\u6E05\u6C64",slug:"\u6E05\u6C64",children:[]}]};export{e as data};
---
+++ assets/index.html.3405cfa5.js
@@ -0,0 +1,1 @@
+const e={key:"v-2341e0f8",path:"/recipes/soup/cold-soup/",title:"\u51B7\u6C64",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[]};export{e as data};
