60f9ed0f25b6498ae0815fba81884139aed69ee7
ziofat <ziofat@users.noreply.github.com>
2022-04-06 13:31:14 +0000

    deploy: 46cbd12ca6519cdbe5648de10bbf7e0f609bd39f

---
+++ assets/recipes.html.b11ce005.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.eee826fd.js";const e={};function _(c,t){return null}var s=r(e,[["render",_]]);export{s as default};
--- assets/recipes.html.b37825ab.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app.014d3776.js";const e={};function _(c,t){return null}var s=r(e,[["render",_]]);export{s as default};
